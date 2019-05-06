e490b14f68511dfbf2df4a9c4077b898e288047c
Shane Walsh <walsh.shane@icloud.com>
2019-05-06 21:27:41 +0100

    Implement Dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import DashBoard from './components/DashBoard';
 
 function App() {
   return (
-  
+    <div>
+      <DashBoard />
+    </div>
   );
 }
 
