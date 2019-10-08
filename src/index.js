7378dbf9fadbfd609e3dec84aa432ce2d6f6ff32
Shane Walsh <walsh.shane@icloud.com>
2019-10-08 19:16:58 +0100

    Refactor routes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import Headlines from './components/Headlines';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>, document.getElementById('root'));
+
+if (module.hot) { 
+  module.hot.accept(); 
+}
 
 serviceWorker.unregister();
