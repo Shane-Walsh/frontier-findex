89960c1274c84b5ccef73cee052290b7c7da9426
Shane Walsh <walsh.shane@icloud.com>
2019-10-08 20:00:30 +0100

    Hide headlines component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import HeadLines from './API/News/headlines';
-//import MostActive from './API/mostActive';
+//import HeadLines from './components/Headlines';
 import DashBoard from './components/DashBoard';
 
 function App() {
   return (
     <div>
       <DashBoard />
-      <HeadLines />
     </div>
   );
 }
