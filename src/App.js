3298bc1a17b8dd80bdaf623749c2d29360d54dfd
System Administrator <root@Shane-Walshs-MacBook-Pro-2.local>
2019-10-15 13:49:37 +0100

    Structure LandingPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import LandingPage from './containers/LandingPage';
+import LandingPage from './containers/LandingPage/LandingPage';
 //import HeadLines from './API/News/headlines';
 //import MostActive from './API/mostActive';
 //import DashBoard from './components/DashBoard';
